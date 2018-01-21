9d1f420d6b7b1bc6cb973e1c4f38cbcd9c48f5f9
Sebastian Brieschenk <s.brieschenk@gmail.com>
2018-01-21 17:08:07 +0100

    restructuring

--- start.js
+++ start.js
@@ -1,30 +1,12 @@
 const { prompt } = require('./lib/prompt');
 const { cloneRepos } = require('./lib/clone-repos');
 const { createWPConfig } = require('./lib/create-wpconfig');
+const { zip } = require('./lib/zip');
 const gDir = dir => require('path').join(__dirname, '/' + dir);
-const zipFolder = require('zip-folder');
-const fs = require('fs-extra');
 let wpConfTmp;
 
-// !cleaned up ? clean up : go on
 const app = prompt
 	.then(wpconfig => (wpConfTmp = wpconfig))
 	.then(cloneRepos)
 	.then(() => createWPConfig(gDir('tmp/wordpress'), wpConfTmp))
-	.then(() => {
-		zipFolder(gDir('tmp'), gDir('headless-wp.zip'), function(err) {
-			if (err) {
-				console.log('oh no!', err);
-			} else {
-				console.log('EXCELLENT! ziped ! fin');
-				fs
-					.remove(gDir('tmp'))
-					.then(() => {
-						console.log('removed tmp folder');
-					})
-					.catch(err => {
-						console.error(err);
-					});
-			}
-		});
-	});
+	.then(zip);
